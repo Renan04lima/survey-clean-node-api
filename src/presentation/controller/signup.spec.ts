00c59fe3ecef8e5ec2ea9091a6582638634072c7
Renan Oliveira <renan04lima@outlook.com>
2021-06-14 17:00:59 -0300

    refactor: simplify test

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -1,8 +1,13 @@
+import { MissingParamError } from '../errors'
 import SignUpController from './signup'
+// sut: system under test
+function makeSut (): SignUpController {
+  return new SignUpController()
+}
 
 describe('SignUp Controller', () => {
   test('should return 400 if no name is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
@@ -11,6 +16,6 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name '))
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
 })
