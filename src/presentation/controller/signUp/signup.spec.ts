5703f4cf9c5ca53867bc64ee54086a574c2ebd64
Renan Oliveira <renan04lima@outlook.com>
2021-07-12 23:47:59 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controller/signUp/signup.spec.ts
+++ src/presentation/controller/signUp/signup.spec.ts
@@ -122,6 +122,25 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new ServerError('error'))
   })
 
+  test('should return 200 if valid data is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    })
+  })
+
   test('should call AddAccount with correct values', async () => {
     const { sut, addAccountStub } = makeSut()
     const addSpy = jest.spyOn(addAccountStub, 'add')
