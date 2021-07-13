4623781a5087d5a42fea2e76b46bb3d7870ebf56
Renan Oliveira <renan04lima@outlook.com>
2021-07-12 23:25:12 -0300

    fix: status code

--- src/presentation/controller/signUp/signup.spec.ts
+++ src/presentation/controller/signUp/signup.spec.ts
@@ -118,7 +118,7 @@ describe('SignUp Controller', () => {
       }
     }
     const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError('error'))
   })
 
