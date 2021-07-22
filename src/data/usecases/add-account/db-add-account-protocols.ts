68844dec11587649fa5f431a3d0bbcbd040e502d
Renan Oliveira <renan04lima@outlook.com>
2021-07-22 11:18:10 -0300

    feat: ensure DbAddAccount calls  AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,3 +1,4 @@
 export * from '@/domain/usecases/add-account'
 export * from '@/domain/model/account'
 export * from '@/data/protocols/encrypt'
+export * from '@/data/protocols/add-account-repository'
