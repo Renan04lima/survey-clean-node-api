6fb4d21d52014c66f7472c3e806f7faf699496f4
Renan Oliveira <renan04lima@outlook.com>
2021-08-10 09:01:41 -0300

    feat: ensure mongodb reconnect if client is down

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -4,7 +4,7 @@ import { AddAccountModel } from '@/domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 export class AccountMongoRepository implements AddAccountRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
 
     const result = await accountCollection.insertOne(accountData)
 
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -3,8 +3,11 @@ import { Collection, MongoClient } from 'mongodb'
 export const MongoHelper = {
   // forçando o typescript não conflitar com a sintaxe dos objetos do js
   client: null as MongoClient,
+  // singleton: retorna um objeto direto, ou seja, não criamos várias instâncias dessa classe
+  uri: null as string,
 
   async connect(uri: string): Promise<void> {
+    this.uri = uri
     this.client = await MongoClient.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true
@@ -12,10 +15,15 @@ export const MongoHelper = {
   },
 
   async disconnect(): Promise<void> {
-    this.client.close()
+    await this.client.close()
+    this.client = null
   },
 
-  getCollection(name: string): Collection {
+  async getCollection(name: string): Promise<Collection> {
+    if (!this.client?.isConnected()) {
+      await this.connect(this.uri)
+    }
+
     return this.client.db().collection(name)
   },
 
