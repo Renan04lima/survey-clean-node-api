b358c0a53a8b194fbbf4c1302734cf2206525b09
Renan Oliveira <renan04lima@outlook.com>
2021-08-18 23:31:36 -0300

    feat: ensure LoginController retturns 400 if no email is provided

---
+++ src/presentation/controller/login/login.ts
@@ -0,0 +1,9 @@
+import { MissingParamError } from '@/presentation/errors'
+import { badRequest } from '@/presentation/helpers'
+import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
+
+export class LoginController implements Controller {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
+  }
+}
