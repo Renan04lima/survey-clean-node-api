21e5efb0ccbe8c516268854977ddbd8bb5bf4715
Renan Oliveira <renan04lima@outlook.com>
2021-08-27 19:39:40 -0300

    feat: ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

---
+++ src/data/protocols/load-account-by-email-repository.ts
@@ -0,0 +1,5 @@
+import { AccountModel } from "@/domain/model/account";
+
+export interface LoadAccountByEmailRepository {
+  loadByEmail: (email: string)=> Promise<AccountModel>
+}
---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,14 @@
+import { LoadAccountByEmailRepository } from "@/data/protocols/load-account-by-email-repository";
+import { Authentication, AuthenticationModel } from "@/domain/usecases/authentication";
+
+export class DbAuthentication implements Authentication {
+  private readonly loadAccountByEmailRepository:LoadAccountByEmailRepository
+  constructor(loadAccountByEmailRepository:LoadAccountByEmailRepository){
+    this.loadAccountByEmailRepository = loadAccountByEmailRepository
+  }
+  async auth (authentication: AuthenticationModel) : Promise<string>{
+    await this.loadAccountByEmailRepository.loadByEmail(authentication.email)
+    return null
+  }
+
+}
