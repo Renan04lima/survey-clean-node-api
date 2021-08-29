4e31cca0892242d55feab1a234c4f7999bb37001
Renan Oliveira <renan04lima@outlook.com>
2021-08-29 16:53:35 -0300

    feat: ensure  DbAuthentication calls Encrypter with correct id

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,3 +1,4 @@
+import { Encrypter } from "@/data/protocols/criptography/encrypt";
 import { HashComparer } from "@/data/protocols/criptography/hash-comparer";
 import { LoadAccountByEmailRepository } from "@/data/protocols/db/load-account-by-email-repository";
 import { Authentication, AuthenticationModel } from "@/domain/usecases/authentication";
@@ -6,14 +7,17 @@ export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository:LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
 
-  constructor(loadAccountByEmailRepository:LoadAccountByEmailRepository, hashComparer: HashComparer){
+  constructor(loadAccountByEmailRepository:LoadAccountByEmailRepository, hashComparer: HashComparer, private readonly encrypter: Encrypter,){
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashComparer = hashComparer
   }
   async auth (authentication: AuthenticationModel) : Promise<string>{
     const account = await this.loadAccountByEmailRepository.loadByEmail(authentication.email)
-    if(account)
-    await this.hashComparer.compare(authentication.password, account.password)
+    if(account){
+      await this.hashComparer.compare(authentication.password, account.password)
+
+      await this.encrypter.encrypt(account.id)
+    }
     
     return null
   }
