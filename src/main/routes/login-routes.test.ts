841026cb4ff2a7982ba7703201b1a807e1b0b69c
Renan Oliveira <renan04lima@outlook.com>
2021-09-04 16:51:38 -0300

    test: login route returns 401 with invalid credencials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -45,4 +45,13 @@ describe('Login Routes', () => {
         }).expect(200)
     })
   })
+  test('Should return 401 on login', async () => {
+    await request(app)
+      .post('/api/login')
+      .send({
+        email: 'rodrigo.manguinho@gmail.com',
+        password: '123'
+      })
+      .expect(401)
+  })
 })
