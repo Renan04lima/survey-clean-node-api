293c0d1c3d128fa6c36cdc0d0ae45f07ae4c08d4
Renan Oliveira <renan04lima@outlook.com>
2021-09-29 23:26:31 -0300

    refactor: separate factories in folders

---
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -0,0 +1,12 @@
+
+import SignUpController from '@/presentation/controllers/signUp/signup-controller'
+import { Controller } from '@/presentation/protocols'
+import { makeSignUpValidation } from './signup-validation-factory'
+import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory'
+import { makeDbAddAccount } from '../../usecases/add-account/db-add-account-factory'
+import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
+
+export const makeSignUpController = (): Controller => {
+  const controller = new SignUpController(makeDbAddAccount(), makeSignUpValidation(), makeDbAuthentication())
+  return makeLogControllerDecorator(controller)
+}
--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { adapterRouter } from '../adapters/express/express-route-adapter'
-import { makeSignUpController } from '../factories/signup/signup-factory'
-import { makeLoginController } from '../factories/login/login-factory'
+import { makeSignUpController } from '../factories/controllers/signup/signup-controller-factory'
+import { makeLoginController } from '../factories/controllers/login/login-controller-factory'
 
 export default (router: Router): void => {
   router.post('/signup', adapterRouter(makeSignUpController()))
