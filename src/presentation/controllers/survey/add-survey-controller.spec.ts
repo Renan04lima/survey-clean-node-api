84ff1f22357f8c806a7ae70f4f301e1f0f27ec94
renan04lima <renan04lima@outlook.com>
2021-10-02 21:46:54 -0300

    test: ensure AddSurveyController returns 400 if Validation fails

--- src/presentation/controllers/survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey-controller.spec.ts
@@ -1,3 +1,4 @@
+import { badRequest } from '@/presentation/helpers'
 import { HttpRequest, Validation } from '@/presentation/protocols'
 import { AddSurveyController } from './add-survey-controller'
 
@@ -39,8 +40,16 @@ describe('Add Survey Controller', () => {
     const { sut, validationStub } = makeSut()
     const validateSpy = jest.spyOn(validationStub, 'validate')
 
-    await sut.handle(makeFakeRequest())
+    console.log(await sut.handle(makeFakeRequest()))
 
     expect(validateSpy).toHaveBeenCalledWith(makeFakeRequest().body)
+  })
+  test('should return 400 if Validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse).toEqual(badRequest(new Error()))
   })
 })
