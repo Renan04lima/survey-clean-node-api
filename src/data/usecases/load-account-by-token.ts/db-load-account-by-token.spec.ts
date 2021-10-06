db957cd0f0a212b9d0937790c2ff18f6e1e82d5f
renan04lima <renan04lima@outlook.com>
2021-10-06 00:01:33 -0300

    test: ensure DbLoadAccountByToken returns null if Decrypter returns null

--- src/data/usecases/load-account-by-token.ts/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token.ts/db-load-account-by-token.spec.ts
@@ -27,4 +27,11 @@ describe('DbLoadAccountByToken Usecase', () => {
     await sut.load('any_token')
     expect(decryptSpy).toHaveBeenCalledWith('any_token')
   })
+
+  test('Should return null if Decrypter returns null', async () => {
+    const { sut, decrypterStub } = makeSut()
+    jest.spyOn(decrypterStub, 'decrypt').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+    const account = await sut.load('any_token', 'any_role')
+    expect(account).toBeNull()
+  })
 })
