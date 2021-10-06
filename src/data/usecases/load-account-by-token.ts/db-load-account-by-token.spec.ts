9e040322fd34e1ff0fa7505f6c8885929c9c3518
renan04lima <renan04lima@outlook.com>
2021-10-06 00:11:24 -0300

    test: ensure DbLoadAccountByToken  throws if Decrypter throws

--- src/data/usecases/load-account-by-token.ts/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token.ts/db-load-account-by-token.spec.ts
@@ -74,4 +74,11 @@ describe('DbLoadAccountByToken Usecase', () => {
     const account = await sut.load('any_token', 'any_role')
     expect(account).toEqual(makeFakeAccount())
   })
+
+  test('Should throw if Decrypter throws', async () => {
+    const { sut, decrypterStub } = makeSut()
+    jest.spyOn(decrypterStub, 'decrypt').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.load('any_token', 'any_role')
+    await expect(promise).rejects.toThrow()
+  })
 })
