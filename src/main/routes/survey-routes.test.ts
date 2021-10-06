66907e50c7a6dc9bef0452ffa6fa60c429d7c5b7
renan04lima <renan04lima@outlook.com>
2021-10-06 09:20:28 -0300

    test: ensure add survey route returns 403 on add survey without accessToken

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -25,6 +25,21 @@ describe('Survey Routes', () => {
   })
 
   describe('POST /surveys', () => {
+    test('Should return 403 on add survey without accessToken', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          }, {
+            answer: 'Answer 2'
+          }]
+        })
+        .expect(403)
+    })
+
     test('Should return 204 on add survey with valid accessToken', async () => {
       const res = await accountCollection.insertOne({
         name: 'Rodrigo',
