ff52d093a4bfe213b214134370aa081fc6e83462
renan04lima <renan04lima@outlook.com>
2021-10-09 20:36:57 -0300

    feat: ensure load surveys route returns 403 if no access token is provided

---
+++ src/main/factories/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -0,0 +1,9 @@
+import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
+import { makeDbLoadSurveys } from '@/main/factories/usecases/survey/load-surveys/db-load-surveys-factory'
+import { LoadSurveysController } from '@/presentation/controllers/survey/load-surveys/load-surveys-controller'
+import { Controller } from '@/presentation/protocols'
+
+export const makeLoadSurveysController = (): Controller => {
+  const controller = new LoadSurveysController(makeDbLoadSurveys())
+  return makeLogControllerDecorator(controller)
+}
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -2,9 +2,12 @@ import { Router } from 'express'
 import { adaptMiddleware } from '../adapters/express/express-middleware-adapter'
 import { adapterRouter } from '../adapters/express/express-route-adapter'
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
+import { makeLoadSurveysController } from '../factories/controllers/survey/load-surveys/load-surveys-controller'
 import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 
 export default (router: Router): void => {
   const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
+  const auth = adaptMiddleware(makeAuthMiddleware())
   router.post('/surveys', adminAuth, adapterRouter(makeAddSurveyController()))
+  router.get('/surveys', auth, adapterRouter(makeLoadSurveysController()))
 }
