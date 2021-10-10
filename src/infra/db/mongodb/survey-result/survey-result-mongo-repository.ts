9468cf33813d26839adfce0b747dd20d4a2649be
renan04lima <renan04lima@outlook.com>
2021-10-10 15:56:58 -0300

    feat: ensure SurveyResultMongoRepository create or update survey result

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,23 @@
+import { SaveSurveyResultRepository } from '@/data/protocols/db/survey-result/save-survey-result-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { SurveyResultModel } from '@/domain/models/survey-result'
+import { SaveSurveyResultModel } from '@/domain/usecases/survey-result/save-survey-result'
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+    const res = await surveyResultCollection.findOneAndUpdate({
+      surveyId: data.surveyId,
+      accountId: data.accountId
+    }, {
+      $set: {
+        answer: data.answer,
+        date: data.date
+      }
+    }, {
+      upsert: true, // NOTE - se não encontrar o mongo criar esses dados acuma
+      returnOriginal: false // NOTE - retorna o valor modificado
+    })
+    return res.value && MongoHelper.map(res.value)
+  }
+}
