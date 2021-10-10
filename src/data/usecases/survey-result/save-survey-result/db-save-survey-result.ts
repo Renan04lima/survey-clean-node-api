f1716045367604ea4264aeba23380294ea4dc6ea
renan04lima <renan04lima@outlook.com>
2021-10-10 11:55:32 -0300

    feat: add DbSaveSurveyResult.save() method and SaveSurveyResultRepository protocol

---
+++ src/data/usecases/survey-result/save-survey-result/db-save-survey-result.ts
@@ -0,0 +1,12 @@
+import { SaveSurveyResultRepository } from '@/data/protocols/db/survey-result/save-survey-result-repository'
+import { SurveyResultModel } from '@/domain/models/survey-result'
+import { SaveSurveyResult, SaveSurveyResultModel } from '@/domain/usecases/survey-result/save-survey-result'
+
+export class DbSaveSurveyResult implements SaveSurveyResult {
+  constructor (private readonly saveSurveyResultRepository: SaveSurveyResultRepository) {}
+
+  async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyResult = await this.saveSurveyResultRepository.save(data)
+    return surveyResult
+  }
+}
