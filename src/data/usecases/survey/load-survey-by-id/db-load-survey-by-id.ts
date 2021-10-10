ab70c796233a2b5752b4cefcc30630294a59f513
renan04lima <renan04lima@outlook.com>
2021-10-10 11:32:08 -0300

    feat: ensure DbLoadSurveyById calls LoadSurveyByIdRepository

---
+++ src/data/usecases/survey/load-survey-by-id/db-load-survey-by-id.ts
@@ -0,0 +1,12 @@
+import { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id'
+import { SurveyModel } from '@/domain/models/survey'
+import { LoadSurveyById } from '@/domain/usecases/survey/load-survey-by-id'
+
+export class DbLoadSurveyById implements LoadSurveyById {
+  constructor (private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository) {}
+
+  async loadById(id: string): Promise<SurveyModel> {
+    await this.loadSurveyByIdRepository.loadById(id)
+    return null
+  }
+}
