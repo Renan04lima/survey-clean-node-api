6f90e181cf48c0daa6d0987ccb76802a51dcfe00
renan04lima <renan04lima@outlook.com>
2021-10-14 21:32:33 -0300

    refactor: add types to AddSurvey usecase

--- src/data/protocols/db/survey/add-survey-repository.ts
+++ src/data/protocols/db/survey/add-survey-repository.ts
@@ -1,5 +1,9 @@
-import { AddSurveyParams } from '@/domain/usecases'
+import { SurveyModel } from '@/domain/models'
 
 export interface AddSurveyRepository {
-  add: (data: AddSurveyParams) => Promise<void>
+  add: (data: AddSurveyRepository.Params) => Promise<void>
+}
+
+export namespace AddSurveyRepository {
+  export type Params = Omit<SurveyModel, 'id'>
 }
