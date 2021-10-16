57d9d47935aed329438a65972c971f15c9936def
renan04lima <renan04lima@outlook.com>
2021-10-16 14:18:31 -0300

    feat: add signup mutation

--- src/main/graphql/resolvers/login.ts
+++ src/main/graphql/resolvers/login.ts
@@ -1,9 +1,13 @@
 import { adaptResolver } from '@/main/adapters'
-import { makeLoginController } from '@/main/factories'
+import { makeLoginController, makeSignUpController } from '@/main/factories'
 
 export default {
   Query: {
     login: async (parent: any, args: any) => adaptResolver(makeLoginController(), args)
+  },
+
+  Mutation: {
+    signup: async (parent: any, args: any) => adaptResolver(makeSignUpController(), args)
   }
 
 }
--- src/main/graphql/type-defs/login.ts
+++ src/main/graphql/type-defs/login.ts
@@ -5,6 +5,10 @@ export default gql`
     login (email: String!, password: String!): Account!
   }
 
+extend type Mutation {
+    signup (name: String!, email: String!, password: String!, passwordConfirmation: String!): Account!
+  }
+
   type Account {
     accessToken: String!
     name: String!
