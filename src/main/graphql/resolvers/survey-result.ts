f3943e81afeac78387136bebb26bb9d200165940
renan04lima <renan04lima@outlook.com>
2021-10-16 17:00:49 -0300

    feat: add saveSurveyResult mutation

--- src/main/graphql/resolvers/survey-result.ts
+++ src/main/graphql/resolvers/survey-result.ts
@@ -1,9 +1,13 @@
 import { adaptResolver } from '@/main/adapters'
-import { makeLoadSurveyResultController } from '@/main/factories'
+import { makeLoadSurveyResultController, makeSaveSurveyResultController } from '@/main/factories'
 
 export default {
   Query: {
     surveyResult: async (parent: any, args: any, context: any) => adaptResolver(makeLoadSurveyResultController(), args)
+  },
+
+  Mutation: {
+    saveSurveyResult: async (parent: any, args: any) => adaptResolver(makeSaveSurveyResultController(), args)
   }
 
 }
--- src/main/graphql/type-defs/survey-result.ts
+++ src/main/graphql/type-defs/survey-result.ts
@@ -4,6 +4,11 @@ export default gql`
   extend type Query {
     surveyResult (surveyId: String!): SurveyResult! @auth
   }
+
+  extend type Mutation {
+    saveSurveyResult (surveyId: String!, answer: String!): SurveyResult! @auth
+  }
+  
   type SurveyResult {
     surveyId: String!
     question: String!
